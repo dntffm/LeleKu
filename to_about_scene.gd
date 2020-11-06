4bfc64047fb33d5fb27b879137a68cfb10088028
dntffm <dentamauuulana99@gmail.com>
2020-11-06 23:56:55 +0700

    Add Feature sprint 1

---
+++ to_about_scene.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+
+
+
+func _on_AboutBtn_pressed():
+	get_tree().change_scene("res://AboutScene.tscn")
---
+++ to_the_game.gd
@@ -0,0 +1,6 @@
+extends TextureButton
+
+
+
+func _on_StartBtn_pressed():
+	get_tree().change_scene("res://Pendederan.tscn")
